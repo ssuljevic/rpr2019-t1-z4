73fcdfa1accaf55a0b394265588fd418e0a0f1e3
Semir Suljević <ssuljevic1@etf.unsa.ba>
2019-10-17 00:13:31 +0200

    1. Verzija: nije vršeno testiranje

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,53 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private int brojArtikalaNaStanju;
+    Artikl[] artikli = new Artikl[50];
+
+    Korpa(){
+        brojArtikalaNaStanju = 0;
+        for(int i = 0; i < artikli.length; i++){
+            artikli[i] = new Artikl();
+        }
+    }
+
+    public boolean dodajArtikl(Artikl artikl){
+        if( brojArtikalaNaStanju == artikli.length - 1){
+            return false;
+        }
+        artikli[brojArtikalaNaStanju] = artikl;
+        brojArtikalaNaStanju = brojArtikalaNaStanju + 1;
+        return true;
+    }
+
+    public int getBrojArtikalaNaStanju() {
+        return brojArtikalaNaStanju;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl zaVratit = new Artikl();
+        Zavrsi: for(int i = 0; i < artikli.length; i++){
+            if( artikli[i].getKod().equals(kod)){
+                zaVratit = artikli[i];
+                artikli[i] = null;
+                for(int j = i; j < artikli.length - 1; j++){
+                    artikli[j] = artikli[j + 1];
+                }
+                // mozda treba postavit na null ?
+                artikli[artikli.length - 1] = new Artikl();
+                break Zavrsi;
+            }
+        }
+        return zaVratit;
+    }
+    public int dajUkupnuCijenuArtikala(){
+        int vrati = 0;
+        for(int i = 0; i < brojArtikalaNaStanju; i++){
+            vrati = vrati + artikli[i].getCijena();
+        }
+        return vrati;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,47 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private int brojArtikalaNaStanju;
+    private Artikl[] artikli = new Artikl[1000];
+
+    // konstruktor
+    Supermarket() {
+        this.brojArtikalaNaStanju = 0;
+        for(int i = 0; i < artikli.length; i++){
+            artikli[i] = new Artikl();
+        }
+    }
+
+    public  void dodajArtikl(Artikl artikl){
+
+        if( brojArtikalaNaStanju == artikli.length) {
+            // treba zabraniti dodavanje novih artikala npr
+            //try catch blok
+        }
+
+        artikli[brojArtikalaNaStanju] = artikl;
+        brojArtikalaNaStanju = brojArtikalaNaStanju + 1;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod){
+       Artikl zaVratit = new Artikl();
+        Zavrsi: for(int i = 0; i < artikli.length; i++){
+            if( artikli[i].getKod().equals(kod)){
+                zaVratit = artikli[i];
+                artikli[i] = null;
+                for(int j = i; j < artikli.length - 1; j++){
+                    artikli[j] = artikli[j + 1];
+                }
+                // mozda treba postavit na null ?
+                artikli[artikli.length - 1] = new Artikl();
+                break Zavrsi;
+            }
+        }
+        brojArtikalaNaStanju = brojArtikalaNaStanju - 1;
+       return zaVratit;
+    }
 }
